5b8cf02a7301bbcd5f4196bf1777cba6ab855991
RogerAfonso <r.afonso.rfa@gmail.com>
2022-03-30 15:40:41 -0300

    estudos js

---
+++ introducao-js/js/main.js
@@ -0,0 +1,16 @@
+//alert("Meu primeiro js")
+
+
+var nome = "Roger de Oliveira Afonso";
+var idade = 42;
+var idade2 = 20;
+var frase = "Senegal se classificou para a copa do mundo!!"
+
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Senegal", "Egito"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
---
+++ lista e dicionario/js/main.js
@@ -0,0 +1,35 @@
+
+//------------------------------------LISTAS--------------------------------------
+
+/*var lista = ["maça", "pera", "laranja"];
+//console.log(lista);
+
+lista.push("uva"); //add à lista
+//console.log(lista);
+
+lista.pop(); //remove último elemento da lista
+//console.log(lista);
+//console.log(lista.length);// mostra a qtde de elementos que tem na lista
+
+//console.log(lista.reverse());// mostra os elementos na ordem contrária
+
+
+console.log(lista);
+//console.log(lista.toString()[0]);//muda de array para string
+console.log(lista.join("|"));//altera separadores da lista*/
+
+
+//---------------------------------DICIONARIO-------------------------------------
+
+//var fruta = {nome:"maçã", cor:"vermelha"};
+//console.log(fruta.nome);
+
+var fruta = [{nome:"maçã", cor:"vermelha"}, {nome:"laranja", cor:"alaranjado"},{nome:"kiwi", cor:"verde"}];
+
+console.log(fruta[1].nome);
+console.log(fruta[0].cor);
+
+
+
+
+
