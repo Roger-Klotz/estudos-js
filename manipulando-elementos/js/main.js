a89060854ef5a5cae549985d41489aa6bbf9def9
RogerAfonso <r.afonso.rfa@gmail.com>
2022-03-31 00:29:09 -0300

    estudos js

--- manipulando-elementos/js/main.js
+++ manipulando-elementos/js/main.js
@@ -1,1 +1,17 @@
-func
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = <b>"Obrigado por clicar"</b>;
+}
+
+function redirecionar(){
+    window.open("https://globallab.org/en/#.YkUX2yjMLrc");
+}
+
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+
+}
+
+function voltar(){
+    document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+}
