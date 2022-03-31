a5c9fbcf7e8ddd1197757a346e57c48b1b5d9260
RogerAfonso <r.afonso.rfa@gmail.com>
2022-03-30 23:50:32 -0300

    estudos js

--- condicionais e laços de repetição/js/main.js
+++ condicionais e laços de repetição/js/main.js
@@ -1,8 +1,34 @@
 
+//-----------------------------------IF---------------------------
 
-var idade = prompt("Qual a sua idade:");
+/*var idade = prompt("Qual a sua idade:");
 if (idade >= 18){
     alert("maior de idade");
 } else{
     alert("menor de idade");
-}
+}*/
+
+
+//-----------------------------------WHILE------------------------
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+
+//---------------------------------------FOR----------------------------
+
+/*var count;
+for(count = 0; count <= 5; count ++){
+    alert(count);
+}*/
+
+//---------------------------------------DATE--------------------------------
+
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
